e9edcdea7bee7ecb5ca53e449eec1eb1c783a916
Netetra <netetra07@gmail.com>
2025-01-13 23:37:31 +0900

    feat: エンコーダによるパルスのカウント

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-use defmt::info;
-// Print panic message to probe console
-use panic_probe as _;
-
 use cortex_m_rt::entry;
+use defmt::*;
 use defmt_rtt as _;
+use panic_probe as _;
 use stm32f3xx_hal::{delay::Delay, pac, prelude::*};
 
 #[allow(clippy::empty_loop)]
@@ -15,23 +12,42 @@ use stm32f3xx_hal::{delay::Delay, pac, prelude::*};
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
+
+    // クロック供給有効化
+    dp.RCC.apb1enr.write(|w| w.tim2en().enabled());
+
     let mut rcc = dp.RCC.constrain();
     let mut flash = dp.FLASH.constrain();
-    let clocks = rcc.cfgr.sysclk(8u32.MHz()).freeze(&mut flash.acr);
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let clocks = rcc
+        .cfgr
+        .sysclk(8.MHz())
+        .pclk1(1.MHz())
+        .freeze(&mut flash.acr);
+
+    // TIM2設定
+    // エンコーダモードに設定
+    dp.TIM2.smcr.write(|w| w.sms().encoder_mode_1());
+    // 設定を反映
+    dp.TIM2.egr.write(|w| w.ug().update());
+
+    // TIM2有効化
+    dp.TIM2.cr1.write(|w| w.cen().enabled());
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    let _encoder_a =
+        gpioa
+            .pa0
+            .into_af_push_pull::<1>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+    let _encoder_b =
+        gpioa
+            .pa1
+            .into_af_push_pull::<1>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
 
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    let mut led = gpiob
-        .pb3
-        .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-
     loop {
-        led.set_high().unwrap();
-        info!("High");
-        Delay::delay_ms(&mut delay, 1000u32);
-        led.set_low().unwrap();
-        info!("Low");
-        Delay::delay_ms(&mut delay, 1000u32);
+        let count = dp.TIM2.cnt.read().cnt().bits();
+        info!("Count: {}", count);
+        delay.delay_ms(100u32);
     }
 }
